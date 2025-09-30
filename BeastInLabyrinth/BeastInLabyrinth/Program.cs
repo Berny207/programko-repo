7177dd58e15f31407f0462008ab1296dd298c6c8
Jan Bernat <jan.bernat@outlook.cz>
2025-09-30 20:16:15 +0200

    Commited a commit from 2 commits ago

--- BeastInLabyrinth/BeastInLabyrinth/Program.cs
+++ BeastInLabyrinth/BeastInLabyrinth/Program.cs
@@ -1,135 +1,167 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using static System.Net.Mime.MediaTypeNames;
-
+﻿using BeastInLabyrinth;
+using System;
+using System.Collections.Generic;
+using static Util.Util;
 
 namespace BeastInLabyrinth
 {
-    public class Prisera
-    {
-        public int x { get; set; }
-        public int y { get; set; }
 
-        /// <summary>
-        /// sets the right hand relative vectors based on monster's direction
-        /// <para>direction 0 = pravaRuka[0] and so on</para>
-        /// </summary>
-        private int pravaRuka = { {1, 0},{0, 1},{-1, 0},{0, -1} }
-        /// <summary>
-        /// 0 = up
-        /// <para>1 = right</para>
-        /// 2 = down
-        /// <para>3 = left</para>
-        /// </summary>
-        public int direction { get; set; }
+	public class Prisera
+	{
+		public int x { get; set; }
+		public int y { get; set; }
+		/// <summary>
+		/// 0 = up
+		/// <para>1 = right</para>
+		/// 2 = down
+		/// <para>3 = left</para>
+		/// </summary>
+		public int direction { get; set; }
+		/// <summary>
+		/// monster's movement options
+		/// </summary>
+		private int[,] pohybovaMatice = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+		private char[] znaceni = { '^', '>', 'v', '<' };
+		private bool pohybVpred = false;
+		public void Krok(Bludiste bludiste)
+		{
+			bludiste.radky[y][x] = '.';
+			if (pohybVpred == true)
+			{
+				pohybVpred = false;
+				x = x + pohybovaMatice[direction, 0];
+				y = y + pohybovaMatice[direction, 1];
 
-        public void krok(Bludiste bludiste)
-        {
-            
-            //pokud mas zed po prave strane a misto pred sebou, jdi dopredu
-            //pokud mas zed po prave strane a zed pred sebou, otoc se doleva
-            //pokud nemas zed po prave strane, otoc se doprava a udelej krok dopredu(zde muzu vzdy udelat krok dopredu protoze vim, ze tam neni zed)
+			}
+			else
+			{
+				bool jeZedPred = JeZed(bludiste, 0);
+				bool jeZedVpravo = JeZed(bludiste, 1);
+				if (jeZedVpravo == false)
+				{
+					pohybVpred = true;
+					direction = Modulo((direction + 1), 4);
+				}
+				if (jeZedPred == true && jeZedVpravo == true)
+				{
+					direction = Modulo((direction - 1), 4);
+				}
+				if (jeZedPred == false && jeZedVpravo == true)
+				{
+					x = x + pohybovaMatice[direction, 0];
+					y = y + pohybovaMatice[direction, 1];
+				}
+			}
+			bludiste.radky[y][x] = znaceni[direction];
+		}
 
-            //zadny jiny pripad nemuze nastat
-        }
+		/// <summary>
+		/// returns if there is a wall around the monster in target direction
+		/// <para>Direction is the same as monster's direction</para>
+		/// </summary>
+		/// <param name="bludiste"></param>
+		/// <param name="smer"></param>
+		/// <returns></returns>
+		public bool JeZed(Bludiste bludiste, int smer)
+		{
+			int pravaRukaX = x + pohybovaMatice[(direction + smer) % (pohybovaMatice.Length / 2), 0];
+			int pravaRukaY = y + pohybovaMatice[(direction + smer) % (pohybovaMatice.Length / 2), 1];
+			if (bludiste.radky[pravaRukaY][pravaRukaX] == 'X')
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+}
+public class Bludiste
+{
+	public int WIDTH { get; set; }
+	public int HEIGHT { get; set; }
+
+	public List<List<char>> radky = new List<List<char>>();
+
+	public Prisera najdiPriseru()
+	{
+		Prisera prisera = new Prisera();
+		for (int y = 0; y < HEIGHT; y++)
+		{
+			for (int x = 0; x < WIDTH; x++)
+			{
+				if (radky[y][x] != '.' && radky[y][x] != 'X')
+				{
+					prisera.x = x;
+					prisera.y = y;
+					switch (radky[y][x])
+					{
+						case '^':
+							prisera.direction = 0;
+							break;
+						case '>':
+							prisera.direction = 1;
+							break;
+						case 'v':
+							prisera.direction = 2;
+							break;
+						case '<':
+							prisera.direction = 3;
+							break;
+					}
+				}
+			}
+		}
+		return prisera;
+	}
+
+	public void vytiskniBludiste()
+	{
+		foreach (List<char> radek in radky)
+		{
+			Console.WriteLine(radek.ToArray());
+		}
+		Console.WriteLine();
+	}
+}
 
 
-        public bool jeZedPoPraveRuce(Bludiste bludiste)
-        {
-            private int pravaRukaX = x + pravaRuka[direction][0];
-		    private int pravaRukaY = y + pravaRuka[direction][1];
-            if(bludiste.get(pravaRukaX).get(pravaRukaY) == 'X')
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-	    }
-    }
-    public class Bludiste
-    {
-        public int WIDTH { get; set; }
-        public int HEIGHT { get; set; }
 
-        public List<List<char>> radky = new List<List<char>>();
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Bludiste bludiste = new Bludiste();
+		bludiste.WIDTH = int.Parse(Console.ReadLine());
+		bludiste.HEIGHT = int.Parse(Console.ReadLine());
+		string loadedLine;
+		for (int i = 0; i < bludiste.HEIGHT; i++)
+		{
+			bludiste.radky.Add(Console.ReadLine().ToList());
+		}
 
-        public Prisera najdiPriseru()
-        {
-            Prisera prisera = new Prisera();
-            for(int y = 0; y < HEIGHT; y++)
-            {
-                for(int x = 0;x < WIDTH; x++)
-                {
-                    if(radky[y][x] != '.' && radky[y][x] != 'X')
-                    {
-                        prisera.x = x;
-                        prisera.y = y;
-                        switch (radky[y][x])
-                        {
-                            case '^':
-                                prisera.direction = 0;
-                                break;
-                            case '>':
-                                prisera.direction = 1;
-                                break;
-                            case 'v':
-                                prisera.direction = 2;
-                                break;
-                            case '<':
-                                prisera.direction = 3;
-                                break;
-                        }
-                    }
-                }
-            }
-            return prisera;
-        }
+		Prisera prisera = bludiste.najdiPriseru();
 
-        public void vytiskniBludiste()
-        {
-            foreach (List<char> radek in radky)
-            {
-                Console.WriteLine(radek.ToArray());
-            }
-        }
-    }
+		int N = 20;
+		for (int i = 0; i < N; i++)
+		{
+			prisera.Krok(bludiste);
+			bludiste.vytiskniBludiste();
+		}
+	}
+}
 
 
-
-    internal class Program
-    {
-        static public string MoveUpDirectory(string path)
-        {
-            if(path == null)
-            {
-                return path;
-            }
-            while (path[path.Length - 1] != '\\')
-            {
-                path = path.Remove(path.Length - 1);
-            }
-            return path;
-        }
-
-        static void Main(string[] args)
-        {
-
-            // get StreamReader for loading from file
-            string inputPath = MoveUpDirectory(System.Environment.ProcessPath)+"bludiste.txt";
-            StreamReader loader = new StreamReader(inputPath);
-
-            Bludiste bludiste = new Bludiste();
-            bludiste.WIDTH = int.Parse(loader.ReadLine());
-            bludiste.HEIGHT = int.Parse(loader.ReadLine());
-            string loadedLine;
-            while((loadedLine = loader.ReadLine()) != null)
-            {
-                bludiste.radky.Add(loadedLine.ToList());
-            }
-            bludiste.vytiskniBludiste();
-            Prisera prisera = bludiste.najdiPriseru();
-        }
-    }
+namespace Util
+{
+	public static class Util
+	{
+		public static int Modulo(int num1, int num2)
+		{
+			//we add n times num2 to num1, where n is |num1|.num2+1. +1 because we are rounding down, new num1 > 0
+			num1 = num1 + num2 * (Math.Abs(num1) / num2 + 1);
+			return num1 % num2;
+		}
+	}
 }
