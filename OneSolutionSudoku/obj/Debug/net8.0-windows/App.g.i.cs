a32ced5ed457cb9bc4bd813e604b296f1e41e4b0
Jan Bernat <jan.bernat@outlook.cz>
2025-11-14 15:36:19 +0100

    pls commit

--- OneSolutionSudoku/obj/Debug/net8.0-windows/App.g.i.cs
+++ OneSolutionSudoku/obj/Debug/net8.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace OneSolutionSudoku {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace OneSolutionSudoku {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public static void Main() {
             OneSolutionSudoku.App app = new OneSolutionSudoku.App();
             app.InitializeComponent();
--- OneSolutionSudoku/obj/Debug/net8.0-windows/Options_Page.g.i.cs
+++ OneSolutionSudoku/obj/Debug/net8.0-windows/Options_Page.g.i.cs
@@ -55,13 +55,13 @@ namespace OneSolutionSudoku {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/OneSolutionSudoku;component/options_page.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/OneSolutionSudoku;V1.0.0.0;component/options_page.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Options_Page.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -71,7 +71,7 @@ namespace OneSolutionSudoku {
         }
         
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.10.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "9.0.11.0")]
         [System.ComponentModel.EditorBrowsableAttribute(System.ComponentModel.EditorBrowsableState.Never)]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Design", "CA1033:InterfaceMethodsShouldBeCallableByChildTypes")]
         [System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Maintainability", "CA1502:AvoidExcessiveComplexity")]
