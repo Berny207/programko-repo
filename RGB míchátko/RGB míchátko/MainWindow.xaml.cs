397a6ebcda5add9e0e9bc6d7236b0b61c42086e8
Jan Bernat <jan.bernat@outlook.cz>
2026-01-07 18:43:39 +0100

    Finished color mixer

--- RGB míchátko/RGB míchátko/MainWindow.xaml.cs
+++ RGB míchátko/RGB míchátko/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Diagnostics.Eventing.Reader;
+using System.Reflection;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
@@ -17,15 +19,18 @@ namespace RGB_míchátko
     /// </summary>
     public partial class MainWindow : Window
     {
+        bool loaded = false;
         public MainWindow()
         {
             InitializeComponent();
+            loaded = true;
             Rectangle.Fill = new SolidColorBrush(Color.FromRgb(0, 0, 0));
             
         }
         private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
-            byte red = Convert.ToByte(double.Round(SliderRed.Value));
+            if(loaded == false) { return; }
+			byte red = Convert.ToByte(double.Round(SliderRed.Value));
             byte green = Convert.ToByte(double.Round(SliderGreen.Value));
             byte blue = Convert.ToByte(double.Round(SliderBlue.Value));
             TextBoxRed.Text = red.ToString();
@@ -47,6 +52,32 @@ namespace RGB_míchátko
 
         private void TextChanged(object sender, TextChangedEventArgs e)
         {
-        }
+            if(loaded == false) { return; }
+			byte red = 0;
+            byte green = 0;
+            byte blue = 0;
+			TextBox source = (TextBox)sender;
+            if (source.Name == "TextBoxRed")
+            {
+                updateTextBox(source, SliderRed);
+			}
+            else if (source.Name == "TextBoxGreen")
+            {
+				updateTextBox(source, SliderGreen);
+			}
+            else if (source.Name == "TextBoxBlue")
+            {
+				updateTextBox(source, SliderBlue);
+			}
+		}
+
+        private void updateTextBox(TextBox source, Slider boundSlider)
+        {
+			if (source.Text == "") { return; }
+			long input = Convert.ToUInt32(source.Text);
+			if (input > 255) { input = 255; source.Text = "255"; }
+			boundSlider.Value = input;
+            SliderValueChanged(source, null);
+		}
     }
 }
