67bce4fa9d456e2f764c4a58c879902db687ab2b
Jan Bernat <jan.bernat@outlook.cz>
2026-04-12 19:02:40 +0200

    Finished maturitní otázka 23

--- nejdelsi posloupnost/nejdelsi posloupnost/obj/Debug/net8.0/nejdelsi posloupnost.AssemblyInfo.cs
+++ nejdelsi posloupnost/nejdelsi posloupnost/obj/Debug/net8.0/nejdelsi posloupnost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nejdelsi posloupnost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f67445b48ab23dd94117367cb8f3437ccad8128c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb2e5b7e109e5402ebc49aa6291ff9f6dc0be089")]
 [assembly: System.Reflection.AssemblyProductAttribute("nejdelsi posloupnost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nejdelsi posloupnost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
